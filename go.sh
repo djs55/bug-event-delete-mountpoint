9c169f07b389c7537a228fa169a17bad1b346e23
David Scott <dave.scott@docker.com>
2019-12-02 20:40:49 +0000

    Use a script to fill in the compose file
    Signed-off-by: David Scott <dave.scott@docker.com>

--- go.sh
+++ go.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+set -e
 
-rm -f compose/webapp/new_file
-mkdir -p compose/webapp
-echo initial-contents > compose/webapp/some_existing_file
+cd compose
+
+# Fill in the path in the compose file
+cat docker-compose.yml.in | sed "s|COMPOSEDIR|$(pwd)|g" > docker-compose.yml
+# Set up the initial contents
+rm -f webapp/new_file
+mkdir -p webapp
+echo initial-contents > webapp/some_existing_file
 
 function finish {
-	(cd compose && docker-compose down) || true
+	docker-compose down || true
 }
 trap finish EXIT
 
 # Starting application
-(cd compose && docker-compose up -d)
+docker-compose up -d
 sleep 1s # just in case
 
 echo The container should have a populated /webapp/node_modules directory:
@@ -20,14 +26,14 @@ echo The container should show 2 mountpoints:
 docker exec compose_webapp_1 mount | grep webapp
 
 echo Touching some_existing_file and new_file.
-touch ./compose/webapp/some_existing_file
-touch ./compose/webapp/new_file
+touch ./webapp/some_existing_file
+touch ./webapp/new_file
 
 echo The container should see the 2 new files:
 docker exec compose_webapp_1 ls -l /webapp
 
 echo Modifying some_existing_file on the host.
-echo modified > ./compose/webapp/some_existing_file
+echo modified > ./webapp/some_existing_file
 
 echo The container should see the modification:
 docker exec compose_webapp_1 cat /webapp/some_existing_file
@@ -37,3 +43,4 @@ docker exec compose_webapp_1 ls -lsa ./webapp/node_modules
 
 echo The container should show 2 mountpoints:
 docker exec compose_webapp_1 mount | grep webapp
+
